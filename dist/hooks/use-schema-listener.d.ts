048659d9d8f61c62b5e89712a3298d9d58231e09
neki-dev <dev@neki.guru>
2024-08-19 02:23:19 +0200

    fix(*): rebuild dist

--- dist/hooks/use-schema-listener.d.ts
+++ dist/hooks/use-schema-listener.d.ts
@@ -1,8 +1,8 @@
 import type { MapSchema, SetSchema } from "@colyseus/schema";
 type Callbacks<T> = {
-    onAdd?: (item: T) => void;
-    onChange?: (item: T) => void;
-    onRemove?: (item: T) => void;
+    onAdd?: (item: T, key: string) => void;
+    onChange?: (item: T, key: string) => void;
+    onRemove?: (item: T, key: string) => void;
 };
-export declare function useSchemaListener<T>(schema: MapSchema<T> | SetSchema<T>, callbacks: Callbacks<T>): void;
+export declare function useSchemaListener<T>(schema: MapSchema<T> | SetSchema<T>, { onAdd, onChange, onRemove }: Callbacks<T>, depends?: any[]): void;
 export {};
